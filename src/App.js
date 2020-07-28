5b027abe624d1e795ce54eb12f372f3fe48db24e
Alex <alex.sousa20@hotmail.com>
2020-07-27 22:35:31 -0300

    Deploy da app

--- src/App.js
+++ src/App.js
@@ -1,12 +1,44 @@
 import React from 'react';
 import Menu from './components/Menu';
-
+import dadosInicias from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carousel from './components/Carousel';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div >
-        <Menu/>
+    <div style={{background: "#141414"}}>
+      <Menu />
+
+      <BannerMain 
+        videoTitle={dadosInicias.categorias[0].videos[0].titulo}
+        url={dadosInicias.categorias[0].videos[0].url}
+        videoDescription={"O que é Front-end"}
+      />
+
+      
+      
+      <Carousel
+        ignoreFirstVideo
+        category={dadosInicias.categorias[0]}
+      
+      />
+
+      <Carousel
+        category={dadosInicias.categorias[1]}
+      />
+      <Carousel
+        category={dadosInicias.categorias[2]}
+      />
+      <Carousel
+        category={dadosInicias.categorias[3]}
+      />
+      <Carousel
+        category={dadosInicias.categorias[4]}
+      />
+
         
+      
     </div>
   );
 }
--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,22 +1,22 @@
 import React from 'react';
 import Logo from '../../assets/img/lexflix-blue.png';
 import './menu.css';
-import ButtonLink from './components/ButtonLink';
+import Button from '../Button';
 
+// import ButtonLink from './components/ButtonLink';
 
-function Menu () {
-    return(
-        <nav className="Menu">
-            <a href="/"> 
-                <img className="Logo" src={Logo} alt="LexFlix"/>
-            </a>
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="LexFlix" />
+      </a>
 
-            <ButtonLink className="ButtonLink" href="/">
-                Novo Vídeo
-            </ButtonLink>
-           
-        </nav>
-    )
+      <Button as="a" className="ButtonLink" href="/">
+        Novo Vídeo
+      </Button>
+    </nav>
+  );
 }
 
 export default Menu;
