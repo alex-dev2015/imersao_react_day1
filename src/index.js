c77d6770423401f9803e5b994e03155d07d17350
AlexSousa <alex.sousa20@hotmail.com>
2020-07-28 18:02:08 -0300

    Criando diretórios page

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import CadastroVideo from './pages/cadastro/Video'
+import { BrowserRouter, Switch, Route } from 'react-router-dom';
 
+const Pagina404 = () => (<div>Página 404 </div>)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    
+    <Switch>
+      <Route path="/cadastro/video" component={CadastroVideo} />
+      <Route path="/" component={App} exact />
+      <Route  component={Pagina404}  />
+
+      
+    </Switch>
+    
+  </BrowserRouter>,
+  
+  // <React.StrictMode>
+  //   <App />
+  // </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
 
