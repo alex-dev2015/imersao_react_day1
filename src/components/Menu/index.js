1ef9a2677e8499935ca3c4ccd75ad66141885f1a
AlexSousa <alex.sousa20@hotmail.com>
2020-07-29 10:32:02 -0300

    Ajustes no Carousel

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Logo from '../../assets/img/lexflix-blue.png';
+import {Link} from 'react-router-dom';
 import './menu.css';
 import Button from '../Button';
 
@@ -8,11 +9,11 @@ import Button from '../Button';
 function Menu() {
   return (
     <nav className="Menu">
-      <a href="/">
+      <Link to="/">
         <img className="Logo" src={Logo} alt="LexFlix" />
-      </a>
+      </Link>
 
-      <Button as="a" className="ButtonLink" href="/">
+      <Button as={Link} className="ButtonLink" to="/cadastro/video">
         Novo Vídeo
       </Button>
     </nav>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import Home from './pages/Home';
 import CadastroVideo from './pages/cadastro/Video'
+import CadastroCategoria from './pages/cadastro/Categoria'
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import Footer from './components/Footer';
 
 const Pagina404 = () => (<div>Página 404 </div>)
 
@@ -11,8 +13,9 @@ ReactDOM.render(
   <BrowserRouter>
     
     <Switch>
+      <Route path="/" component={Home} exact />
       <Route path="/cadastro/video" component={CadastroVideo} />
-      <Route path="/" component={App} exact />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route  component={Pagina404}  />
 
       
